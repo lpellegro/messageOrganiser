60ba3c9c80669ec1c69e5058f3ce94c6829af6b6
Luca Pellegrini <88320330+lpellegro@users.noreply.github.com>
2022-10-25 15:38:01 +0200

    Update sms.py

--- sms.py
+++ sms.py
@@ -4,54 +4,6 @@ import json
 import logging
 from restApi import *
 
-#hostlogger = logging.getLogger('main.whatsapp')
-
-def sms(messageFrom, messageBody, messageTo, link, bearer, fromField):
-  print(messageBody)
-  print("SIAMO IN SMS-SSETE")
-  url = "https://api-sandbox.imiconnect.io/v1/sms/messages"
-  payload = json.dumps({
-    "contentType": "text",
-    "content": f"from: {messageFrom}\n{messageBody}\ncheck this link: {link}",
-    "from": fromField,
-    "to": messageTo,
-  })
-
-  headers = {
-    'Authorization': bearer, 
-    'Content-Type': 'application/json',
-
-  }
-
-  response = requests.request("POST", url, headers=headers, data=payload)
-  print(messageTo)
-  print(type(messageTo))
-  print(response.text)
-  #hostlogger.info(f"sms response is {response.text}")
-
-def whatsappete(messageFrom, messageBody, to, link, bearer, fromField):
-  print(messageBody)
-  print("SIAMO IN WHATSAPPETE")
-  url = "https://api-sandbox.imiconnect.io/v1/whatsapp/messages"
-  payload = json.dumps({
-    "contentType": "text",
-    "content": f"from: {messageFrom}\n{messageBody}\ncheck this link: {link}",
-    "from": fromField, 
-    "to": to,
-  })
-  headers = {
-    'Authorization': bearer,
-    'Content-Type': 'application/json',
-
-  }
-
-  response = requests.request("POST", url, headers=headers, data=payload)
-  #hostlogger.info(f"whatsapp response is {response.text}")
-  print(to)
-  print(type(to))
-  print(response.text)
-
-
 def smsLegacy(messageFrom, messageBody, link, to):
   baseUrl = "https://api.us.webexconnect.io/resources/v1/"
   imiHeaders = headersInRequest("notUsed", "json")
@@ -71,10 +23,6 @@ def smsLegacy(messageFrom, messageBody, link, to):
       }
     ]
   }
-  #headers = {
-    #'Content-type': 'application/json',
-    #'key': '6d41c47e-3f10-11ed-9e4c-02b6c62f50eb' #'6d41c47e-3f10-11ed-9e4c-02b6c62f50eb'
- # }
 
   print("SENDING SMS TO: ", to)
   imiRequest = api(baseUrl)
@@ -82,12 +30,4 @@ def smsLegacy(messageFrom, messageBody, link, to):
   response = imiRequest.response
   print("Response for the SMS is: ", response)
 
-  #response = requests.request("POST", baseUrl, headers=headers, data=payload)
-
-  #print(response.text)
-
-#smstest("Luca Pellegrini", "Need to test SMS with an US mobile, ciao!", "+19016322594")
-
-#sms("test", "test", "+393357561685", "link", "ae2b87ba-a6c1-11ec-b58d-063d0d6fdfb5", "+393399950808")
-#whatsappete("test", "test", "+393357561685", "link", "ae2b87ba-a6c1-11ec-b58d-063d0d6fdfb5","a_163115952236765903")
 
